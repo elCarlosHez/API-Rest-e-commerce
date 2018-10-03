d4af4f8b65607b625397577925b1af54e2e6e3e6
Luis Arellano <luis.harellano@alumnos.udg.mx>
2018-10-03 17:45:47 -0500

    Added the methods and properties of the controllers

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    const PRODUCT_AVAILABLE = 'disponible';
+    const PRODUCT_NO_AVAILABLE = 'no disponible';
+    protected $fillable = [
+        'name',
+        'description',
+        'quantify',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailable()
+    {
+        return $this->status == Product::PRODUCT_AVAILABLE;
+    }
 }
