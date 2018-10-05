0e5da62d35f7819ea0dfba2b74df94ccbd088dbf
Luis Arellano <luis.harellano@alumnos.udg.mx>
2018-10-05 17:09:47 -0500

    Created all user Controller methods

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         $usuarios = User::all();
-        return response()->json(['data' => $usuarios], 299);
+        return response()->json(['data' => $usuarios], 200);
     }
 
     /**
@@ -37,7 +37,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ];
+
+        $this->validate($request, $rules);
+
+        $campos = $request->all();
+        $campos['password'] = bcrypt($request->password);
+        $campos['verified'] = User::NOT_VERIFIED_USER;
+        $campos['verification_token'] = User::generateVerificationToken();
+        $campos['admin'] = User::REGULAR_USER;
+        $usuario = User::create($campos);
+
+        return response()->json(['data' => $usuario], 201);
     }
 
     /**
@@ -48,7 +63,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $usuario = User::findOrFail($id);
+        return response()->json(['data' => $usuario], 200);
     }
 
     /**
@@ -71,7 +87,41 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $rules = [
+            'email' => 'email|unique:users,email,' . $user->id,
+            'password' => 'min:6|confirmed',
+            'admin' => 'in:' . User::ADMIN_USER . ',' . User::REGULAR_USER,
+        ];
+
+        $this->validate($request, $rules);
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+        if ($request->has('email') && $user->email != $request->email) {
+            $user->verified = User::NOT_VERIFIED_USER;
+            $user->verification_token = User::generateVerificationToken();
+            $user->email = $request->email;
+        }
+        if ($request->has('password')) {
+            $user->password = $request->password;
+        }
+
+        if ($request->has('admin')) {
+            if (!$user->isVerified()) {
+                return response()->json(['error' => 'Unicamente los usuarios verificados pueden cambiar su valor de administrador', 'code' => 409], 409);
+            }
+            $user->admin = $request->admin;
+        }
+
+        if (!$user->isDirty()) {
+            return response()->json(['error' => 'Se debe especificar al menos un valor diferente para actualizar', 'code' => 409], 409);
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user], 200);
     }
 
     /**
@@ -82,6 +132,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json(['data' => $user], 200);
     }
 }
