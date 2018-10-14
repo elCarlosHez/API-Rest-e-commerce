c314dc5ac3b8819129c28adef751723d9d0eccf6
Luis Arellano <luis.harellano@alumnos.udg.mx>
2018-10-14 12:55:29 -0500

    Create new routes for Product - Consult buyers of a product - Consult Transactions from a buyer of a product - Consult Categories of a product - Consult transactions of a product * Now is posible to create new products * Now is possible to create transactions of a product - If a product get out of inventory, its status change to not available

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+use App\Category;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product, Category $category)
+    {
+        // sync: Delete all previus categories and add the new category 
+        // attach: Do not verify if the new category already exist
+        // syncWithoutDetaching: Just append the new category, if this already exist "nothing" happen
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product, Category $category)
+    {
+        if (!$product->categories()->find($category->id)) {
+            return $this->errorResponse(
+                'La categoría especificada no es una categoría de este producto',
+                404
+            );
+        }
+
+        $product->categories()->detach([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
+}
