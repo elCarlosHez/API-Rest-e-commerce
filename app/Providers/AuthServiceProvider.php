20a81677cd8ca170d1e22308d02bd4244115e21e
Carlos Arellano <luis.harellano@alumnos.udg.mx>
2018-12-09 10:42:20 -0600

    Created general scopes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -33,5 +33,13 @@ class AuthServiceProvider extends ServiceProvider
         Passport::tokensExpireIn(Carbon::now()->addMinutes(5));
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
         Passport::enableImplicitGrant();
+
+        Passport::tokensCan([
+            'purchase-product' => 'Crear transacciones para comprar productos determinados.',
+            'manage-products' => 'Crear, ver, actualizar y eliminar productos',
+            'manage-account' => 'Obtener la información de la cuenta, nombre, email, estado ( sin contraseña), modificar datos como email, nombre, y contraseña. No puede liminar la cuenta',
+            'read-general' => 'Obtener información general, categorias donse se compra y se vende, productos vendidos o comprados, transacciones, compras y ventas',
+
+        ]);
     }
 }
